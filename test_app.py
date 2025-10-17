026c30d98d101890ff2328c06494f732b28641a8
Emily de Britto <emilybg@al.insper.edu.br>
2025-10-17 15:21:55 -0300

    Adds tests for invalid data and filtering by data

--- test_app.py
+++ test_app.py
@@ -47,6 +47,7 @@ def test_get_products(mock_conectar_bd, client):
             "created_at": "2023-20-02 20:00:00"
         }
     ]
+    
 
 @pytest.mark.parametrize(
     'product_id, mock_result',
@@ -65,9 +66,85 @@ def test_get_product_by_id(client, id, mock_result):
         mock_cursor.fetchone.return_value = mock_result
         mock_conectar_bd.return_value = mock_conn
         response = client.get(f'/products/{id}')
+   
     
     # THEN
         assert response.status_code == 200
         assert response.get_json() == mock_result
 
+def test_get_product_by_id_not_found(client):
+    with patch("app.conectar_bd") as mock_conectar_bd:
+        mock_conn = MagicMock()
+        mock_cursor = MagicMock()
+        mock_conn.cursor.return_value = mock_cursor
+        mock_cursor.fetchone.return_value = None
+        mock_conectar_bd.return_value = mock_conn
+        response = client.get('/products/999999999')
+    # THEN
+        assert response.status_code == 404
+        assert response.get_json() == {"error": "Product not found"}
+
+
+def test_get_products_by_category(client, category_id):
+    with patch("app.conectar_bd") as mock_conectar_bd:
+        mock_conn = MagicMock()
+        mock_cursor = MagicMock()
+        mock_conn.cursor.return_value = mock_cursor
+        mock_cursor.fetchall.return_value = [
+            (0, "Notebook", "Notebook Alienware muito bom", 100000.00, "Usado", 1, 1, "Anunciado", "2023-10-01 10:00:00"),
+        ]
+        mock_conectar_bd.return_value = mock_conn
+        response = client.get('/products/category/1')
+    # THEN
+        assert response.status_code == 200
+        assert response.get_json() == [
+            {
+                "id": 0,
+                "name": "Notebook",
+                "description": "Notebook Alienware muito bom",
+                "price": 100000.00,
+                "state": "Usado",
+                "category_id": 1,
+                "seller_id": 1,
+                "status": "Anunciado",
+                "created_at": "2023-10-01 10:00:00"
+            }
+        ]
+
+
+def test_post_product(client):
+    new_product = {
+        "name": "Tablet",
+        "description": "Tablet Samsung Galaxy Tab S7",
+        "price": 3000.00,
+        "state": "Novo",
+        "category_id": 3,
+        "seller_id": 3,
+        "status": "Anunciado"
+    }
+    with patch("app.conectar_bd") as mock_conectar_bd:
+        mock_conn = MagicMock()
+        mock_cursor = MagicMock()
+        mock_conn.cursor.return_value = mock_cursor
+        mock_conectar_bd.return_value = mock_conn
+        response = client.post('/products', json=new_product)
+    # THEN
+        assert response.status_code == 201
+        assert response.get_json() == {"message": "Produto criado com sucesso"}
+        
+
+def test_post_product_invalid_data(client):
+    invalid_product = {
+        "name": "",
+        "description": "Tablet Samsung Galaxy Tab S7",
+        "price": -3000.00,
+        "state": "Novo",
+        "category_id": 3,
+        "seller_id": 3,
+        "status": "Anunciado"
+    }
+    response = client.post('/products', json=invalid_product)
+    # THEN
+    assert response.status_code == 400
+    assert response.get_json() == {"error": "Dados inválidos"}
     
